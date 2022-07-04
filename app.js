e4edcf7bdaf78738aee6d497a42a47875fbc0c79
Sejin PPark <103471665+Buzzin97@users.noreply.github.com>
2022-07-04 17:05:40 +0900

    const 변수추가
    변수이름은 camel case를 사용한다.

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
  const a = 5;
  const b = 2;
- const MyName = "Sejin"
+ const myName = "Sejin"
  
  console.log(a + b);
  console.log(a * b);
  console.log(a / b);
+ console.log("hello " + myName);
  
