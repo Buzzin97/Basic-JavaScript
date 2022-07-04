18ea37807ee75f9756713216b19e3f53407c350c
Sejin PPark <103471665+Buzzin97@users.noreply.github.com>
2022-07-04 17:31:11 +0900

    Boolean
    true , false
    undefined , null

--- app.js
+++ app.js
@@ -1,13 +1,4 @@
- const a = 5;
- const b = 2;
- let myName = "Sejin"
- 
- console.log(a + b);
- console.log(a * b);
- console.log(a / b);
- console.log("hello " + myName);
-
- myName ="Park Sejin"
-
- console.log("Your new name is " + myName);
+const amIFat = null;
+let someting;
+console.log(someting, amIFat);
  
