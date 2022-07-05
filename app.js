6db335d2f7c947745a17725738ba4e7a9b0a8e9d
Sejin PPark <103471665+Buzzin97@users.noreply.github.com>
2022-07-05 15:37:19 +0900

    여러가지 계산기 만들어보기

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
-    const player = {
-        name: "Seijn",
-        sayHello: function (otherPersonName) {
-            console.log("hello! " +otherPersonName + " nice to meet you");
+    const calculator = {
+        add:function(a ,b) {
+            console.log(a + b);
         },
+        sub:function(a ,b) {
+            console.log(a -b);
+        },
+        div:function(a ,b) {
+            console.log(a / b);
+        },
+        mul:function(a ,b) {
+            console.log(a *b);
+        },
+        squ:function(a ,b) {
+            console.log(a**b);
+        }
     };
 
-    player.sayHello("Sejin");
-    player.sayHello("Park");
+    calculator.add(4, 5);
+    calculator.sub(4, 5);
+    calculator.div(4, 5);
+    calculator.mul(4, 5);
+    calculator.squ(4, 5);
+    
+    
