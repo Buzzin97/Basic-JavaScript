1021a8b37fbb5f660c862aed149e872bec540651
Sejin PPark <103471665+Buzzin97@users.noreply.github.com>
2022-07-05 17:12:56 +0900

    if문과 and or 연산자

--- app.js
+++ app.js
@@ -1,26 +1,15 @@
-    const calculator = {
-        add:function(a ,b) {
-            return a + b;
-        },
-        sub:function(a ,b) {
-            return a -b;
-        },
-        div:function(a ,b) {
-            return a / b;
-        },
-        mul:function(a ,b) {
-            return a * b;
-        },
-        squ:function(a ,b) {
-            return a ** b;
-        },
-    };
-
-    const addResult = calculator.add(2, 3);
-    const subResult = calculator.sub(addResult, 3);
-    const mulResult = calculator.div(2, mulResult);
-    const divResult = calculator.mul(divResult, addResult);
-    const squResult = calculator.squ(divResult, subResult);
-
-
-
+    const age = parseInt( prompt("How old are you? ") );
+    
+    if(isNaN(age) || age < 0){
+        console.log("양의 숫자를 입력해주세요! :)")
+        // condition === true
+    } else if(age < 18) {
+        console.log("당신은 미성년자입니다.")
+    } else if(age >= 18 && age <= 50){
+        console.log("술을 마실 수 있습니다.")
+    } else if(age > 50 && age <=80){
+        console.log("운동을 하시오")
+    } else if(age > 80){
+        console.log("하고 싶은거 다하시오")
+    }
+ 
