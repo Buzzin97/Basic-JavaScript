75c57ca65260cdadcd197c3e5c7b743f96e691a4
Sejin PPark <103471665+Buzzin97@users.noreply.github.com>
2022-07-06 14:12:56 +0900

    Event more

--- app.js
+++ app.js
@@ -1,19 +1,41 @@
-    const title = document.querySelector("div.hello:first-child h1");
+    const h1 = document.querySelector("div.hello:first-child h1");
 
-    function handleTitleClick(){
+    function handleTitleClick() {
         console.log("title was clicked!!");
-        title.style.color ="blue";
+        h1.style.color ="blue";
     }
 
-    function  handleMouseEnter(){
-        title.innerText = "Mouse is here!";
+    function  handleMouseEnter() {
+        h1.innerText = "Mouse is here!";
     }     
 
-    function handleMouseLeave(){
-        title.innerText = "Mouse is gone";
+    function handleMouseLeave() {
+        h1.innerText = "Mouse is gone";
     }
 
-    title.addEventListener("click", handleTitleClick);
-    title.addEventListener("mouseenter", handleMouseEnter);
-    title.addEventListener("mouseleave", handleMouseLeave);
+    function handleWindowResize() {
+        document.body.style.backgroundColor = "tomato";
+    }
+
+    function handleWindowCopy() {
+        alert("copier");
+    }
+
+    function handleWindowOffline() {
+        alert("SOS no WIFI");
+    }
+
+    function handleWindowOnline() {
+        alert("인터넷이 연결되었습니다.");
+    }
+
+
+    h1.onclick = handleTitleClick;
+    h1.onmouseenter = handleMouseEnter;
+    h1.addEventListener("mouseleave", handleMouseLeave);
+
+    window.addEventListener("resize", handleWindowResize);
+    window.addEventListener("copy", handleWindowCopy);
+    window.addEventListener("offline", handleWindowOffline);
+    window.addEventListener("online", handleWindowOnline);
 
