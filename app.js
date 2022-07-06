cdf55773a7d90ae4dd6c1fbfb135de83ddc35aa8
Sejin PPark <103471665+Buzzin97@users.noreply.github.com>
2022-07-06 14:55:42 +0900

    JS로 css 다루기

--- app.js
+++ app.js
@@ -1,41 +1,14 @@
-    const h1 = document.querySelector("div.hello:first-child h1");
+const h1 = document.querySelector("div.hello:first-child h1");
 
-    function handleTitleClick() {
-        console.log("title was clicked!!");
-        h1.style.color ="blue";
+function handleTitleClick() {
+    const currentColor = h1.style.color;
+    let newColor;
+    if(currentColor === "blue"){
+        newColor = "tomato";
+    }else {
+        newColor= "blue"
     }
+    h1.style.color = newColor;
+}
 
-    function  handleMouseEnter() {
-        h1.innerText = "Mouse is here!";
-    }     
-
-    function handleMouseLeave() {
-        h1.innerText = "Mouse is gone";
-    }
-
-    function handleWindowResize() {
-        document.body.style.backgroundColor = "tomato";
-    }
-
-    function handleWindowCopy() {
-        alert("copier");
-    }
-
-    function handleWindowOffline() {
-        alert("SOS no WIFI");
-    }
-
-    function handleWindowOnline() {
-        alert("인터넷이 연결되었습니다.");
-    }
-
-
-    h1.onclick = handleTitleClick;
-    h1.onmouseenter = handleMouseEnter;
-    h1.addEventListener("mouseleave", handleMouseLeave);
-
-    window.addEventListener("resize", handleWindowResize);
-    window.addEventListener("copy", handleWindowCopy);
-    window.addEventListener("offline", handleWindowOffline);
-    window.addEventListener("online", handleWindowOnline);
-
+h1.addEventListener("click", handleTitleClick);
