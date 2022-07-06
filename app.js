2780e710079ce31e414045fbf7b6b944876516ed
Sejin PPark <103471665+Buzzin97@users.noreply.github.com>
2022-07-06 11:32:49 +0900

    Searching For Elements

--- app.js
+++ app.js
@@ -1,15 +1,3 @@
-    const age = parseInt( prompt("How old are you? ") );
-    
-    if(isNaN(age) || age < 0){
-        console.log("양의 숫자를 입력해주세요! :)")
-        // condition === true
-    } else if(age < 18) {
-        console.log("당신은 미성년자입니다.")
-    } else if(age >= 18 && age <= 50){
-        console.log("술을 마실 수 있습니다.")
-    } else if(age > 50 && age <=80){
-        console.log("운동을 하시오")
-    } else if(age > 80){
-        console.log("하고 싶은거 다하시오")
-    }
- 
+    const title = document.querySelector(".hello h1");
+
+    title.innerText = "hello";
