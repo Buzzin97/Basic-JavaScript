98eaa3c74694d58566f383d8ee4570693cde2a4b
Sejin PPark <103471665+Buzzin97@users.noreply.github.com>
2022-07-09 23:04:06 +0900

    Js preventEvent
    alert는 기본적으로 preventEvent 기능이 있습니다. 한번 더 막아주느것이 가능하다!

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
      const loginForm = document.querySelector("#login-form");
      const loginInput = document.querySelector("#login-form input");
 
-qw
-     function onLoginSubmit(info) {
-          info.preventDefault();
+     const link = document.querySelector("a");
+
+     function onLoginSubmit(event) {
+          event.preventDefault();
           console.log(loginInput.value);
      }
 
+     function handleLinkClick(event){
+          event.preventDefault();
+          alert("clicked!");
+     }
+
      loginForm.addEventListener("submit", onLoginSubmit);
+     link.addEventListener("click" , handleLinkClick);
  
      
      
