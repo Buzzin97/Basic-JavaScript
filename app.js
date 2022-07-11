bac60ee3296ae6419214f71de08af5ea9a361199
Sejin PPark <103471665+Buzzin97@users.noreply.github.com>
2022-07-11 16:10:10 +0900

    Js html 수정

--- app.js
+++ app.js
@@ -8,7 +8,7 @@
           event.preventDefault();
           loginForm.classList.add(HIDDEN_CLASSNAME);
           const username = loginInput.value;
-          greeting.innerText = "Hello" + username;
+          greeting.innerText = "Hello " + username;
           greeting.classList.remove(HIDDEN_CLASSNAME);
      }
 
