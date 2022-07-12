01ab50cba1e2df65d83b2f15ff6a06b1ff7835d3
Sejin PPark <103471665+Buzzin97@users.noreply.github.com>
2022-07-12 16:13:04 +0900

    random 이미지
    createElement (js로 html추가하기)
    appendChild (추가하는 기능)

---
+++ js/background.js
@@ -0,0 +1,13 @@
+const images = [
+    "이미지1.jpeg",
+    "이미지2.jpeg",
+    "이미지3.jpeg"
+];
+
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+
+const bgImage = document.createElement("img");
+
+bgImage.src = `img/${chosenImage}`;
+
+document.body.appendChild(bgImage);
