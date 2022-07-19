d9d0a0adcc580933b35a8365d1fe36f973d82070
Sejin PPark <103471665+Buzzin97@users.noreply.github.com>
2022-07-19 16:42:51 +0900

    null 과 udefinded

---
+++ 6.empty.js
@@ -0,0 +1,15 @@
+// null , undefined
+let variable;
+console.log(variable); //undefined
+
+variable = null;
+console.log(variable); //null
+
+let activeItem; // 아직 활성화된 아이템이 있는지 없는지 모르는 상태!
+activeItem = null; // 활성화된 아이템이 없는 상태! (정해진 상태)
+
+console.log(typeof 123);
+console.log(typeof '123');
+
+console.log(typeof null); // --> object가 할당되었다!
+console.log(typeof undefined);
