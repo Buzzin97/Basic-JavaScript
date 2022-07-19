db1f830cfd112d6178f1c03998bbbdce580c3318
Sejin PPark <103471665+Buzzin97@users.noreply.github.com>
2022-07-19 10:26:10 +0900

    날씨 정보가져오기

--- js/weather.js
+++ js/weather.js
@@ -1,7 +1,19 @@
+    const API_KEY = "81b15ea536b511d78bdb3829b5e01c0a";
+    
     function onGeoOk(position){
-        const lat = position.coords.latitiude;
-        const lng  = position.coords.latitiude;
-        console.log("이곳에 있습니다.", lat, lng);
+        const lat = position.coords.latitude;
+        const lon  = position.coords.longitude;
+        const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+        //자바스크립트가 url를 불러온다. <fetch>로 url를 받고 then json값을 response(추출)한다.
+        //추출하면 data값을 얻는다. 
+        fetch(url)
+            .then((response) => response.json())
+            .then((data) => {
+                const weather = document.querySelector("#weather span:first-child");
+                const city = document.querySelector("#weather span:last-child");
+                city.innerText = data.name;
+                weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+            });
     }
 
     function onGeoOkError(){
@@ -11,4 +23,6 @@
 
     // 이 기능은 true 값과 error값 두개가 필요하다
     navigator.geolocation.getCurrentPosition(onGeoOk, onGeoOkError);
+
+    //openweathermap.org에서 날씨값을 가져올 수 있다.
     
