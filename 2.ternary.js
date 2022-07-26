b7b434c213070e2509a06f5dc92fcf9eca69884e
Sejin PPark <x2345x@naver.com>
2022-07-26 13:27:31 +0900

    삼항 연산자
    조건식 ? 참 : 거짓
    
    ex)
    a === b ? true : false

---
+++ 2.ternary.js
@@ -0,0 +1,12 @@
+// 삼항 조건 연산자 Ternary Operator
+// 조건식 ? 참인경우 : 거짓인경우
+let fruit = 'apple';
+if (fruit === 'apple') {
+  console.log('🍎');
+} else {
+  console.log('!!');
+}
+fruit === 'apple' ? console.log('🍎') : console.log('!!');
+
+let emoji = fruit === 'apple' ? '🍎' : '!!';
+console.log(emoji);
