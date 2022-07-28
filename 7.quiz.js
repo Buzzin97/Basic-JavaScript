458300ad89a03580745ce099cdd62c0ba60b20e3
Sejin PPark <x2345x@naver.com>
2022-07-28 16:33:28 +0900

    callback 사용예 + quiz

--- 7.quiz.js
+++ 7.quiz.js
@@ -3,17 +3,26 @@
 // 5, 순회하는 숫자를 다 출력하고 싶음
 // 5, 순회하는 숫자의 두배값을 다 출력하고 싶음
 // function iterate(max, action)
-const max = 6;
-const rotate = function (i) {
-  for(i = 1; i < 6; i++);{
-    console.log(i);
-  }
-}
-console.log(rotate);
-
-
 function iterate(max, action) {
  for(let i = 0; i < max; i++) {
-  action(i);
+  action(i); // 포문이 돌면서 action을 호출한다.
  }
 }
+
+// action 함수 만들기
+function log(num) {
+  console.log(num);
+} // action은 단순히 console.log 해주는 함수
+
+function logDouble(num) {
+  console.log(num * 2);
+} //action은 두배 숫자를 출력해주는 함수
+
+iterate(5,log); // lterate 함수에 인자 할당하기!
+iterate(5, logDouble);
+iterate(5 , (num) => console.log(num * 3));
+
+//callback 함수 사용예
+setTimeout(()=>{
+  console.log('1초뒤 이 함수가 실행됩니다.')
+}, 1000);
