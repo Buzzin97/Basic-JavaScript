c5a34d2e8aea395a9df2ed6531c5098ce4e79d59
Sejin PPark <x2345x@naver.com>
2022-08-07 16:38:07 +0900

    setInterval & toStringLocal
    셋인터벌과 로컬 시간을 가져와서 타이머를 만들었다.

--- 8.quiz.js
+++ 8.quiz.js
@@ -9,5 +9,8 @@ const ids = 'user1, user2, user3, user4';
 const space = ids.split(',');
 console.log(space);
 // 3. 1초에 한번씩 시계를 (날짜포함) 출력해보자!  setInterval
-const now = new Date();
-console.log(now.toLocaleString('ko-Kr'));
+setInterval(() => {
+  const now = new Date();
+  console.log(now.toLocaleString('ko-KR'));
+},1000);
+
