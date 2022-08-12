497feca48fa3ff7b2d308b948b65bd39ce0513de
Sejin PPark <x2345x@naver.com>
2022-08-13 00:01:41 +0900

    3.quiz
    배열2의 배열1의 순환 요소가 포함 되어있을때 result 배열에 배열1의 순환요소를 추가한다.

--- 5.quiz.js
+++ 5.quiz.js
@@ -43,6 +43,10 @@ console.log(counter(fruits,'🥝'));
 
 function match(array1,array2) {
   let result = [];
-  for(let i = 0; i < array1.length; i++)
+  for(let i = 0; i < array1.length; i++){
+    if(array2.includes(array1[i])) {
+      result.push(array1[i])
+    }
+  }
 }
 
