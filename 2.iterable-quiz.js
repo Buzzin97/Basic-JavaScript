f21424f80e4cb5f9d9fea06d0aba4fe0322ab882
Sejin PPark <x2345x@naver.com>
2022-08-18 12:30:12 +0900

    Update 2.iterable-quiz.js

--- 2.iterable-quiz.js
+++ 2.iterable-quiz.js
@@ -3,7 +3,20 @@
 // 0, 1, 2, 3 ...., 9
 // 0, 2, 4, 6 ...., 18
 
+function makeIterable(initialValue, maxValue, callback) {
+  return {
+    [Symbol.iterator]() {
+      let num = initialValue;
+      return {
+        next() {
+          return {value: callback(num++), done: num > maxValue};
+        },
+      };
+    },
+  };
+}
 
+const multiple = makeIterable(0,20, (n) => n * 2);
 for (const num of multiple) {
   console.log(num);
 }
