4486ba6a6afc36caafa5cb83f26604fe143da53c
Sejin PPark <x2345x@naver.com>
2022-08-20 19:20:34 +0900

    Update 4.generator.js

--- 4.generator.js
+++ 4.generator.js
@@ -3,17 +3,17 @@
 // 0, 1, 2, 3 ...., 9
 // 0, 2, 4, 6 ...., 18
 
-const multiple = {
-  [Symbol.iterator]() {
-    const max = 10;
-    let num = 0;
-    return {
-      next() {
-        return{value: num++ *2, done: num > max };
-      }
-    }
-  },
-};
-for (const num of multiple) {
-  console.log(num);
-}
+// const multiple = {
+//   [Symbol.iterator]() {
+//     const max = 10;
+//     let num = 0;
+//     return {
+//       next() {
+//         return{value: num++ *2, done: num > max };
+//       }
+//     }
+//   },
+// };
+// for (const num of multiple) {
+//   console.log(num);
+// }
