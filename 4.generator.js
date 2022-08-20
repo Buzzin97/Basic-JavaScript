7c08f2f447ff5bcac7cd747595119c123af16914
Sejin PPark <x2345x@naver.com>
2022-08-20 20:31:27 +0900

    Update 4.generator.js

--- 4.generator.js
+++ 4.generator.js
@@ -17,3 +17,34 @@
 // for (const num of multiple) {
 //   console.log(num);
 // }
+
+function* multipleGenterator() {
+  for(let i = 0; i < 10; i++) {
+    console.log(i);
+     yield i **2; // 사용자가 next()를 호출할때 까지 기다렸다가 호출하면 순회한다. 
+  }
+}
+const multiple = multipleGenterator();
+let next = multiple.next();
+console.log(next.value, next.done); // 처음 값은 0과 false가 나온다. done이 false니까 순환 가능하다는 뜻이다.
+
+//multiple.return(); // 다음 next()호출전에 return을 하면 정지!
+
+next = multiple.next();
+console.log(next.value, next.done);
+
+const add = addGenerator();
+let nexy2 = add.next();
+function* addGenerator() {
+  try {
+    for(let i = 0; i < 10; i++) {
+      console.log(i);
+      yield i**2;
+    }
+    } catch(error) {
+      console.log(error);
+    }
+  }
+
+
+add.throw('Error!');
