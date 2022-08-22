049a214afaf76f3f6eda4604b14832e28a2f1b97
Sejin PPark <x2345x@naver.com>
2022-08-22 15:39:49 +0900

    Update 3.quiz.js

--- 3.quiz.js
+++ 3.quiz.js
@@ -2,8 +2,18 @@
 const fruits = ['a', 'b' , 'c' , 'a', 'b', 'd'];
 // ['a' , 'b' , 'c' , 'd']
 
+function array (item) {
+  return new Set([...item])
+}
+console.log(array(fruits));
+
+const arr = new Set([...fruits])
+console.log(arr);
+
 // 주어진 두 세트의 공통된 아이템만 담고 있는 세트를 만들기 
 const set1 = new Set([1, 2, 3, 4, 5]);
 const set2 = new Set([1, 2, 3]);
+
+
 
 
