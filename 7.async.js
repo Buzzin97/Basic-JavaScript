9698e2ed65a73132fef0faa573c53f051d8577fd
Sejin PPark <x2345x@naver.com>
2022-08-24 17:04:01 +0900

    Create 7.async.js

---
+++ 7.async.js
@@ -0,0 +1,37 @@
+function getBanana() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve('banana');
+    }, 1000);
+  });
+}
+
+function getApple() {
+  return new Promise ((resolve) => {
+    setTimeout(() => {
+      resolve('apple')
+    }, 3000)
+  })
+}
+
+function getOrange() {
+  return Promise.reject(new Error('no orange'));
+}
+
+// 바나ㅏ와 사과를 같이 가지고 오기
+async function fetchFruits() {
+  const banana = await getBanana();
+  const apple = await getApple();
+  return [banana, apple]
+}
+fetchFruits()
+.then(fruits => console.log(fruits))
+
+
+//   return getBanana() //
+//     .then((banana) =>
+//       getApple() // 
+//         .then((apple) => [banana, apple])
+//     )
+//     .then(console.log)
+// }
