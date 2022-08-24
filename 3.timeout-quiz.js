e8a2efeb53a7e8c86ffc3e0490157823d78ad21f
Sejin PPark <x2345x@naver.com>
2022-08-24 12:46:58 +0900

    Update 3.timeout-quiz.js

--- 3.timeout-quiz.js
+++ 3.timeout-quiz.js
@@ -1,5 +1,14 @@
 // 주어진 seconds(초)가 지나면 callback함수를 호출한다.
 // 단, seconde가 0보다 작으면 에러를 던지자!
-function runInDelay(callback, second) {
-  
+function runInDelay(callback, seconds) {
+  if(!callback) {
+    throw new Error('callback함수를 전달 해야 함')
+  }
+  if(!seconds || seconds < 0) {
+    throw new Error('seconds는 0보다 커야함')
+  }
+  setTimeout(callback, seconds * 1000); 
 }
+try {
+  runInDelay(() => {console.log('뿌')}, 2)
+} catch (error) {}
