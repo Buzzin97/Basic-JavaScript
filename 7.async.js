252b529298189946eb41cbe9626a55ff43117117
Sejin PPark <x2345x@naver.com>
2022-08-25 10:56:04 +0900

    Update 7.async.js

--- 7.async.js
+++ 7.async.js
@@ -18,7 +18,7 @@ function getOrange() {
   return Promise.reject(new Error('no orange'));
 }
 
-// 바나ㅏ와 사과를 같이 가지고 오기
+// 바나나와 사과를 같이 가지고 오기
 async function fetchFruits() {
   const banana = await getBanana();
   const apple = await getApple();
