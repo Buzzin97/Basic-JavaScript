54f3b616bbbed50b7607f1247e9fef29ffe9ddfd
Sejin PPark <x2345x@naver.com>
2022-08-30 12:00:50 +0900

    13번 문제

--- 10_15번.js
+++ 10_15번.js
@@ -61,6 +61,32 @@ const solar = [
   '천왕성',
   '혜왕성'
 ]
+search = (n) => {
+  if(n < 0) {
+    console.log("1이상 수를 입력해주세요");
+  } else if (n === 0) {
+    console.log('태양');
+  } else{
+    console.log(`${n}번째 행성은 ${solar[n-1]}입니다`);
+  }
+}
+search(2)
+
 // 입력을 받을떈 prompt를 사용한다.
 // const n = prompt("몇번째 행성을 원하나요?");
-console.log(solar[n-1]);
+// console.log(solar[n-1]);
+
+// 14번 3의 배수 인가요?
+// 입력으로 랜덤한 숫자 n이 주어집니다.
+// 만약 그 수가 3의 배수라면 '짝'이라는 글자를, 
+// 3의 배수가 아니라면 n을 그대로 출력해 주세요.
+
+ printNumber= (nums) => {
+  if(nums % 3 === 0 && n != 0) {
+    console.log('짝');
+  } else {
+    console.log(nums);
+  }
+ }
+
+ printNumber(4)
