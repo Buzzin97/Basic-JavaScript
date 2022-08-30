c1cddf382b108839a5539c1df57b6e045ed30a51
Sejin PPark <x2345x@naver.com>
2022-08-30 10:54:44 +0900

    Update 10_15번.js

--- 10_15번.js
+++ 10_15번.js
@@ -1,3 +1,4 @@
+// 10번 별찍기
 const star = 5;
 for(let i = 1; i <= star; i++) {
   let tree = ""
@@ -9,3 +10,10 @@ for(let i = 1; i <= star; i++) {
   }
   console.log(tree);
 }
+
+// 11번 1 ~ 100까지 숫자 더하기 for이용
+let s = 0;
+for(let i = 0; i <= 100; i++) {
+ s = s + i
+}
+console.log(s);
