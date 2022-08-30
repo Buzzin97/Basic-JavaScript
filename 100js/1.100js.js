0952e38ffc794766e28231a02cc535842a97302c
Sejin PPark <x2345x@naver.com>
2022-08-30 10:17:00 +0900

    Update 1.100js.js

--- 100js/1.100js.js
+++ 100js/1.100js.js
@@ -117,3 +117,15 @@ console.log(result);
   *****
  *******
 *********
+
+const star = 5;
+for(let i = 1; i <= star; i++) {
+  let tree = ""
+  for(let k = 1; k <= star -i; k++) {
+    tree += " "
+  }
+  for(let j = 1; j <= i*2-1; j++) {
+    tree += "*"
+  }
+  console.log(tree);
+}
