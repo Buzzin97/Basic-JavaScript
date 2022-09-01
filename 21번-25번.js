fdb0eaeef69cef8844951f2521854a2f483bdd26
Sejin PPark <x2345x@naver.com>
2022-09-01 22:50:10 +0900

    26번 객체 출력
    [' '] 접근법
    . 접근법 (변수로 접근 x)

--- 21번-25번.js
+++ 21번-25번.js
@@ -21,3 +21,19 @@ cricleWidth = (n) => {
  return result
 }
 console.log(cricleWidth(5));
+
+// 26번 한글 이름을 입력하면 영어를 반환하게 해주기
+
+const planet = {
+  수성 : 'Mercury', 
+  금성 : 'Venus', 
+  지구 : 'Earth', 
+  화성 : 'Mars', 
+  목성 : 'Jupiter', 
+  토성 : 'Saturn', 
+  천황성 : 'Uranus', 
+  혜왕성 : 'Neptune'
+}
+
+console.log(planet['수성']);
+console.log(planet.수성);
