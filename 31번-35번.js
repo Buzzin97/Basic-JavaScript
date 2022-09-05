56f3763efb11e354bcdd84a659252fcb67522bd0
Sejin PPark <x2345x@naver.com>
2022-09-05 21:10:17 +0900

    35번
    함수를 리턴하는 함수

--- 31번-35번.js
+++ 31번-35번.js
@@ -48,4 +48,18 @@ sorted = unsorted.split(" ").sort(function(a,b){
 
 console.log(sorted);
 
+// 35번 Factory 함수만들기 (2제곱 3제곱 4제곱 만들기)
+function one(n){
+  function two(value){
+     return Math.pow(value, n)
+  }
+  return two;
+}
 
+const a = one(2);
+const b = one(3);
+const c = one(4);
+
+console.log(a(10));
+console.log(b(10));
+console.log(c(10));
