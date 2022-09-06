09b80b67636de42de1d0ede5434754726cebdbc7
Sejin PPark <x2345x@naver.com>
2022-09-06 11:37:31 +0900

    37번 반장선거
    배열을 객체타입으로 저장한다.
    for in문 ( 배열의 모든 원소를 순환할때까지 증가한다.)
    삼항 연산자도 사용했다.
    Object.key() => 객체의 key값을 받아서 배열로 반환해준다.
    reduce() 배열의 원소를 하나하나 순회하여 계속 중첩해준다.
    
    reduce(function(a, b))  함수를 실행시켜서 중첩해줄수 있다.
    result[a] > result[b] ? a : b
    true false값을 a값에 계속 넣어준다 (reduce) 함수는 마지막의 a값을 return한다.
    return 값은 빈 문자열 leader에 넣어주었다.

--- 36번-40번.js
+++ 36번-40번.js
@@ -8,3 +8,22 @@ multiple = (num) => {
 }
 
 multiple(2)
+
+// 37번 반장선거 (문자열을 받고 그 문자열중 가장 많이 적힌사람이 반장으로 뽑힌다.)
+const mate = ['원범', '원범', '혜원', '혜원', '혜원', '혜원', '유진', '유진'];
+let leader = " ";
+const results = {};
+
+for(let index in mate) {
+  let val = mate[index];
+  results[val] = results[val] === undefined ? 1 : results[val] += 1;
+}
+leader = Object.keys(results).reduce(function(a , b) {
+  return results[a] > results[b] ? a : b
+})
+
+console.log(results[leader]);
+console.log(leader);
+
+console.log(`${leader}(이)가 총 ${results[leader]}표로 반장이 되었습니다. `);
+
