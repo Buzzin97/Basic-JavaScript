c262e47ebb9a852fad04347c33ed9513e7208b10
Sejin PPark <x2345x@naver.com>
2022-09-08 15:13:48 +0900

    47번 set 자료형 응용하기
    set 객체는 중복을 허락하지 않는다.

--- 46번 ~ 50번.js
+++ 46번 ~ 50번.js
@@ -13,3 +13,23 @@ arr.forEach((n) => {
   n = Math.floor(total/10)
 })
 console.log(total);
+
+
+// 47번 set자료형 응용(set은 중복을 허락하지 않음)
+const people = {
+  이호준: "01050442903",
+  이호상: "01051442904",
+  이준호: "01050342904",
+  이호준: "01050442903",
+  이준: "01050412904",
+  이호: "01050443904",
+  이호준: "01050442903"
+};  // 중복 제거 해주기 set 객체
+
+let result = new Set();
+console.log(result);
+for(let key in people) {
+  result.add(people[key])
+}
+console.log(result); // 중복 제거된 객체 
+console.log(result.size);
